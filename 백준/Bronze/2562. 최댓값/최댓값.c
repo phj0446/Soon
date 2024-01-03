de7ef3301e58a4c82f7ad73a509b84054094203c
hyejin <146970543+phj0446@users.noreply.github.com>
2024-01-03 09:27:33 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.c
+++ 백준/Bronze/2562. 최댓값/최댓값.c
@@ -1,29 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-	int i;
-	int max = 0;
-	int index = 0;
-	int a[9];
-
-	for (i = 0; i < 9; i++)
+	int a[9], max = 0;
+	for (int i = 0; i < 9; i++)
 	{
 		scanf("%d", &a[i]);
+		max = (max > a[i]) ? max : a[i];
 	}
-
-	for (i = 0; i < 9; i++)
+	for (int i = 0; i < 9; i++)
 	{
-		if (a[i] > max)
+		if (max == a[i])
 		{
-			max = a[i];
-			index = i;
+			printf("%d\n", max);
+			printf("%d", i + 1);
+			break;
 		}
-
 	}
-	printf("%d\n", max);
-	printf("%d", index+1);
 
 
 	return 0;
